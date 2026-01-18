3b0fbd066c393aaad57421474c13f429b490de81
Celes Renata <celes@frameshift.net>
2026-01-18 08:21:52 -0800

    Add dependencies attribute to wyoming-satellite override

--- flake.nix
+++ flake.nix
@@ -61,6 +61,7 @@
                 };
                 build-system = with prev.python3Packages; [ setuptools ];
                 propagatedBuildInputs = with prev.python3Packages; [ pyring-buffer wyoming zeroconf ];
+                dependencies = propagatedBuildInputs;
                 pythonImportsCheck = [ "wyoming_satellite" ];
                 dontCheckRuntimeDeps = true;
                 pythonCatchConflicts = false;
