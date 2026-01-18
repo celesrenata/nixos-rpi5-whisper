754bbb5ac1988618cccc532a692c60bdc31e17e7
Celes Renata <celes@frameshift.net>
2026-01-18 03:56:41 -0800

    event tests

--- modules/wyoming-fix.nix
+++ modules/wyoming-fix.nix
@@ -1,5 +1,18 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  wyoming-1-8-0 = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "wyoming";
+    version = "1.8.0";
+    format = "setuptools";
+    
+    src = pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-kMFsn7fpDLzidwMoBreBbmxjGBI5MganMpj0rU/823Y=";
+    };
+  };
+in
 {
-  # Use wyoming-satellite from unstable to get matching Wyoming protocol version
-  services.wyoming.satellite.package = lib.mkForce inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.wyoming-satellite;
+  services.wyoming.satellite.package = pkgs.wyoming-satellite.overridePythonAttrs (old: {
+    propagatedBuildInputs = builtins.filter (p: p.pname or "" != "wyoming") (old.propagatedBuildInputs or []) ++ [ wyoming-1-8-0 ];
+  });
 }
