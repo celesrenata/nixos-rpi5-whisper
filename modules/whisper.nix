fd7ed56363c5964cbecef3005c00af1c07a5be95
Celes Renata <celes@frameshift.net>
2026-01-18 17:37:59 -0800

    remove upstream services from rpi5 until v3d supports audio generation or faster pis exist

--- modules/whisper.nix
+++ modules/whisper.nix
@@ -12,21 +12,7 @@
         };
       };
     };
-    faster-whisper.package = pkgs-unstable.wyoming-faster-whisper;
-    faster-whisper.servers."gremlin-1" = {
-      enable = true;
-      device = "cpu";
-      model = "Systran/faster-whisper-tiny.en";
-      language = "en";
-      beamSize = 4;
-      uri = "tcp://0.0.0.0:10300";
-    };
-    piper.servers."en" = {
-      enable = true;
-      voice = "en_US-lessac-medium";
-      uri = "tcp://0.0.0.0:10200";
-    };
   };
 
-  networking.firewall.allowedTCPPorts = [ 10200 10300 10500 ];
+  networking.firewall.allowedTCPPorts = [ 10500 ];
 }
